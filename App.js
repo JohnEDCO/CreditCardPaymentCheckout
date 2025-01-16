d50794f1e4fd983122aacb64725f4b073ed87dd9
JohnEDCO <john.edward.cordoba@gmail.com>
2025-01-15 19:26:30 -0500

    [new repo] first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text } from 'react-native';
+
+
+function App() {
+  return (
+    <Text>Credit Card Checkout</Text>
+  );
+}
+
+export default App;
